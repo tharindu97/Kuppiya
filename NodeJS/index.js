a2e5d4c9899e8053442a5556c44714e7ad5941db
tharindu97 <tharindukavishna@gmail.com>
2020-04-01 09:48:09 +0530

    http_request

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 
+//GET Request
+app.get('/',(req,res) => {
+    return res.json('GET Request');
+});
+
+//POST Request
+app.post('/',(req,res) => {
+    return res.json('POST Request');
+});
+
+//PUT Request
+app.put('/',(req,res) => {
+    return res.json('PUT Request');
+});
+
+//DELETE Request
+app.delete('/',(req,res) => {
+    return res.json('DELETE Request');
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
