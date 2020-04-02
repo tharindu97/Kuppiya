cf21767e5d99235b373d08af8ffaaf2a7fd319e7
tharindu97 <tharindukavishna@gmail.com>
2020-04-02 20:58:04 +0530

    nextstep

--- NodeJS/routes/todoList/todoList.js
+++ NodeJS/routes/todoList/todoList.js
@@ -15,12 +15,22 @@ let toDos =[
 ];
 
 const router = Router();
-
+//Get all Todo
 router.get('/todo',(req,res) =>{
     return res.json(toDos);
 });
+
+//Get One Todo By index
 router.get('/todo/:num',(req,res) =>{
     const number = req.params.num;
     return res.json(toDos[number - 1]);
 });
+
+//Get todo by id
+router.get('/getId/:id',(req,res) =>{
+    const id = req.params.id;
+    const todo = toDos.filter(todo => todo.id === id);
+    return res.json(todo);
+});
+
 module.exports = router;
