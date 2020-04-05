981f923db07eb17c542ddd991bb0cb993d6e7768
tharindu97 <tharindukavishna@gmail.com>
2020-04-05 20:03:47 +0530

    create_schema

--- Nodejs_With_Mongoose/index.js
+++ Nodejs_With_Mongoose/index.js
@@ -4,6 +4,9 @@ const app = express();
 
 const mongoUrl = 'mongodb+srv://Thari:Tharindu97@cluster0-ykfr0.azure.mongodb.net/test?retryWrites=true&w=majority';
 
+//Models
+require('./models/todos');
+
 //Connect mongodb
 const connectDB = async () => {
     try {
