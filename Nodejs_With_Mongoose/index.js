1e9b3be86211b890a6ff70d133ad8155aa4df2a5
tharindu97 <tharindukavishna@gmail.com>
2020-04-05 19:15:49 +0530

    connect_mongodb

---
+++ Nodejs_With_Mongoose/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const mongoUrl = 'mongodb+srv://Thari:Tharindu97@cluster0-ykfr0.azure.mongodb.net/test?retryWrites=true&w=majority';
+
+//Connect mongodb
+const connectDB = async () => {
+    try {
+        await mongoose.connect(mongoUrl,
+            { 
+                useUnifiedTopology: true,
+                useNewUrlParser: true
+            });
+        console.log('Connect mongoDB');
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+connectDB();
+
+const PORT = process.env.PORT || 5100
+app.listen(PORT, () =>{
+    console.log(`Server Started at ${PORT}`);
+});
