ca900f429a3c80781c6385915b01c6c9c8391931
tharindu97 <tharindukavishna@gmail.com>
2020-04-06 22:38:48 +0530

    validation

--- Nodejs_With_Mongoose/routes/todos/SaveTodo.js
+++ Nodejs_With_Mongoose/routes/todos/SaveTodo.js
@@ -1,12 +1,22 @@
 const { Router } = require('express');
+const { check, validationResult } = require('express-validator'); 
 const  { model } = require('mongoose');
 const Todos  = model('todos');
 
 const router = new Router();
 
-router.post('/api/save',async(req,res)=>{
+router.post('/api/save',
+[
+    check('title','Title is Required').not().isEmpty(),
+    check('description','Description is Required').not().isEmpty()
+],async(req,res)=>{
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        res.status(400).json({ errors: errors.array() });
+    }
     try {
-        
+
         const body = req.body;
         const newTodo = new Todos(body);
         await newTodo.save();
