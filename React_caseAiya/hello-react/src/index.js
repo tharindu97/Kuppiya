dac95cbb7c2f7af82fc10ea74d18664b658e32da
tharindu97 <tharindukavishna@gmail.com>
2020-04-07 21:45:45 +0530

    session1

---
+++ React_caseAiya/hello-react/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hello(){
+    return (
+        <div>Hello World</div>
+    )
+}
+
+ReactDOM.render(<Hello/>, document.querySelector('#root'));
