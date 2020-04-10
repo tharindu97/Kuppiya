2df61062a2a4d8a25fcddabc4437ec4e928494c1
tharindu97 <tharindukavishna@gmail.com>
2020-04-10 19:16:47 +0530

    Encrypt

--- Anguler_With_Nodejs/server/src/routes/auth/auth.routes.js
+++ Anguler_With_Nodejs/server/src/routes/auth/auth.routes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { check } from "express-validator";
+import bcrypt from 'bcryptjs';
 import authBodyValidator from "../../middlewares/auth/authBodyValidator";
 
 const router = Router();
@@ -17,7 +18,10 @@ router.post('/login', authValidation, authBodyValidator ,async (req,res) =>{
 
 //Register
 router.post('/register', authValidation, authBodyValidator ,async (req,res) =>{
-    
+    let { email, password } = req.body;
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+    res.status(200).json({ email, password });
 });
 
 export default router;
