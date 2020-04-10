b3ef201ec1d097d10cb76da459cb5605f453ab55
tharindu97 <tharindukavishna@gmail.com>
2020-04-10 19:30:51 +0530

    image_generator

--- Anguler_With_Nodejs/server/src/routes/auth/auth.routes.js
+++ Anguler_With_Nodejs/server/src/routes/auth/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { check } from "express-validator";
 import bcrypt from 'bcryptjs';
+import gravatar from 'gravatar';
 import authBodyValidator from "../../middlewares/auth/authBodyValidator";
 
 const router = Router();
@@ -19,9 +20,14 @@ router.post('/login', authValidation, authBodyValidator ,async (req,res) =>{
 //Register
 router.post('/register', authValidation, authBodyValidator ,async (req,res) =>{
     let { email, password } = req.body;
+    const image = gravatar.url(email, {
+        s:'200',
+        r:'pg',
+        d:'mm'
+    });
     const salt = await bcrypt.genSalt(10);
     password = await bcrypt.hash(password, salt);
-    res.status(200).json({ email, password });
+    res.status(200).json({ email, password, image });
 });
 
 export default router;
